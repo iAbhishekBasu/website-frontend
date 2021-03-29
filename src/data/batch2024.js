87a16cb56460f1acc77149e70a31014ebb89ebf6
Abhishek Basu <iabhishekbasu@gmail.com>
2021-03-29 20:01:41 +0530

    added tab for 1st years

---
+++ src/data/batch2024.js
@@ -0,0 +1,50 @@
+const Batch2024 = [
+   {
+      name: "Sagar",
+      post: "Junior member",
+      img: require("../media/batch2023/Arka.jpg"),
+      fb: "https://www.facebook.com/animeshdatts",
+      insta: "https://www.instagram.com/animesh.suman98/",
+      linkedin:
+         "https://www.linkedin.com/in/animesh-datta-60770115b/?originalSubdomain=in",
+      mail: "",
+   },
+   {
+      name: "Arka",
+      post: "Junior member",
+      img: require("../media/batch2023/Sagar.jpg"),
+      fb: "https://www.facebook.com/Abhish3kSunny",
+      insta: "https://instagram.com/abhish3k_sunny/",
+      linkedin: "https://www.linkedin.com/in/zer-0-ne/",
+      mail: "",
+   },
+   {
+      name: "Sayan",
+      post: "Junior member",
+      img: require("../media/batch2023/Sayan.jpg"),
+      fb: "https://www.facebook.com/monsij98",
+      insta: "https://instagram.com/monsij98/",
+      linkedin: "https://www.linkedin.com/in/monsijb/",
+      mail: "",
+   },
+   {
+      name: "Aditi",
+      post: "Junior member",
+      img: require("../media/batch2023/Aditi.jpg"),
+      fb: "https://www.facebook.com/profile.php?id=100006949692977",
+      insta: "https://instagram.com/amitav.507/",
+      linkedin: "https://www.linkedin.com/in/amitava-das-77b411155/",
+      mail: "",
+   },
+   {
+      name: "Vaishali",
+      post: "Junior member",
+      img: require("../media/batch2023/Vaishali.jpg"),
+      fb: "https://www.facebook.com/Prateekee",
+      insta: "https://instagram.com/granger2423/",
+      linkedin: "https://www.linkedin.com/in/prateekee-chatterjee/",
+      mail: "",
+   },
+];
+
+export default Batch2024;
