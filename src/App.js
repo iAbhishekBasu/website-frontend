23c7f32b59a4374f52909e8090dc904300313d4e
Restyled.io <commits@restyled.io>
2021-04-01 13:24:39 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import React,{useState,useEffect} from 'react';
-import { Link, Redirect } from "react-router-dom";
-import { Switch, Route } from 'react-router-dom';
-import Home from "./components/homepage/home.component"
-import Header from "./components/header/header.component"
-import InfoG from "./components/infographics-component/info.component"
-import AboutUs from "./components/about-us/about-us.component"
-import Events from "./components/events-component/events.component"
-import EventDetails from "./components/events-component/event.details.component"
-import Member from "./components/members-component/members.component.js"
-import Footer from "./components/footer/footer.component.js"
-import Alumni from "./components/alumni-component/alumni.component.js"
-import Loader from './components/loader/loader'
 
+import React, {useEffect, useState} from 'react';
+import {Link, Redirect} from "react-router-dom";
+import {Route, Switch} from 'react-router-dom';
+
+import AboutUs from "./components/about-us/about-us.component"
+import Alumni from "./components/alumni-component/alumni.component.js"
+import EventDetails from "./components/events-component/event.details.component"
+import Events from "./components/events-component/events.component"
+import Footer from "./components/footer/footer.component.js"
+import Header from "./components/header/header.component"
+import Home from "./components/homepage/home.component"
+import InfoG from "./components/infographics-component/info.component"
+import Loader from './components/loader/loader'
+import Member from "./components/members-component/members.component.js"
+import logo from './logo.svg';
 
 function App() {
-	const [loading,setLoading]=useState(true);
+  const [loading, setLoading] = useState(true);
 
-	useEffect(()=>{
-		setInterval(() => {
-			setLoading(false)
-		}, 3000);
-	})
+  useEffect(() => { setInterval(() => {setLoading(false)}, 3000); })
 
-	if(loading){
-		return(
+  if (loading) {
+                return(
 			<div>
 				<Loader />
 			</div>
@@ -40,9 +37,11 @@ function App() {
 			  <div className="">
 					<Switch>
 						<Route exact path={["/"]} component={Home} />
-					  <Route exact path={["/events"]} component={Events} />
+					  <Route exact path={["/events"]} component={
+      Events} />
 					  <Route exact path={["/events/:id"]} component={EventDetails} />
-					  <Route exact path={["/members"]} component={Member} />
+					  <Route exact path={["/members"]} component={
+      Member} />
 					  <Route exact path={["/alumni"]} component={Alumni} />
 					</Switch>
 			  </div>
--- src/components/homepage/home.component.js
+++ src/components/homepage/home.component.js
@@ -1,26 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link, Redirect } from "react-router-dom";
-import { Switch, Route } from 'react-router-dom';
-import Landing from "../landing-component/landing.component"
-import Header from "../header/header.component"
-import InfoG from "../infographics-component/info.component"
-import AboutUs from "../about-us/about-us.component"
-import Events from "../events-component/events.component"
-import InfoIEEE from "../info-ieee/info-ieee.component"
+
 import React from "react";
-import PostDataService from "../../services/post.service"
 import Fade from 'react-reveal/Fade';
-import ActivitiesHome from "../home-activities/home-activities.component.js"
-import Stats from "../stats-component/stats.component.js"
-import Insight from "../insight-component/insight.component.js"
-import Team from "../team-component/team.component.js"
-import Testinomials from "../testinomial-component/testinomial.component.js"
+import {Link, Redirect} from "react-router-dom";
+import {Route, Switch} from 'react-router-dom';
+
+import PostDataService from "../../services/post.service"
+import AboutUs from "../about-us/about-us.component"
 import Collab from "../collab-component/collab.component.js"
 import Contact from "../contact-component/contact.component.js"
+import Events from "../events-component/events.component"
+import Header from "../header/header.component"
+import ActivitiesHome from "../home-activities/home-activities.component.js"
+import InfoIEEE from "../info-ieee/info-ieee.component"
+import InfoG from "../infographics-component/info.component"
+import Insight from "../insight-component/insight.component.js"
+import Landing from "../landing-component/landing.component"
 import Loader from '../loader/loader'
+import Stats from "../stats-component/stats.component.js"
+import Team from "../team-component/team.component.js"
+import Testinomials from "../testinomial-component/testinomial.component.js"
 
-
-function Home (){
+function Home() {
 
       return (
 		  
